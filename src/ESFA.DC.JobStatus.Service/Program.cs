44adbfc6e6dad45ebd9fdfd1ffa1c6442085077d
John Youren <john.youren@gmail.com>
2018-06-11 15:40:20 +0100

    Initial web job commit

--- src/ESFA.DC.JobStatus.Service/Program.cs
+++ src/ESFA.DC.JobStatus.Service/Program.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using ESFA.DC.JobStatus.Dto;
+using ESFA.DC.JobStatus.Interface;
+using ESFA.DC.JobStatus.Service.Configuration;
+using ESFA.DC.JobStatus.WebServiceCall.Service;
+using ESFA.DC.Logging;
+using ESFA.DC.Logging.Config;
+using ESFA.DC.Logging.Config.Interfaces;
+using ESFA.DC.Logging.Enums;
+using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.Queueing;
+using ESFA.DC.Queueing.Interface;
+using ESFA.DC.Serialization.Interfaces;
+using ESFA.DC.Serialization.Json;
+using Microsoft.Extensions.Configuration;
+using ExecutionContext = ESFA.DC.Logging.ExecutionContext;
 
 namespace ESFA.DC.JobStatus.Service
 {
-    class Program
+    public static class Program
     {
+#if DEBUG
+        private const string ConfigFile = "privatesettings.json";
+#else
+        private const string ConfigFile = "appsettings.json";
+#endif
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(ConfigFile);
+
+            IConfiguration configuration = configBuilder.Build();
+
+            IJobStatusWebServiceCallServiceConfig auditingPersistenceServiceConfig = new JobStatusWebServiceCallServiceConfig(configuration["jobSchedulerApiEndPoint"]);
+            IQueueConfiguration queueConfiguration = new JobStatusQueueConfiguration(configuration["queueConnectionString"], configuration["queueName"], 1);
+            ISerializationService serializationService = new JsonSerializationService();
+            IApplicationLoggerSettings applicationLoggerOutputSettings = new ApplicationLoggerSettings
+            {
+                ApplicationLoggerOutputSettingsCollection = new List<IApplicationLoggerOutputSettings>
+                {
+                    new MsSqlServerApplicationLoggerOutputSettings
+                    {
+                        ConnectionString = configuration["loggerConnectionString"],
+                        MinimumLogLevel = LogLevel.Information
+                    },
+                    new ConsoleApplicationLoggerOutputSettings
+                    {
+                        MinimumLogLevel = LogLevel.Information
+                    }
+                },
+                TaskKey = "Audit",
+                EnableInternalLogs = true,
+                JobId = "Audit Service",
+                MinimumLogLevel = LogLevel.Information
+            };
+            IExecutionContext executionContext = new ExecutionContext
+            {
+                JobId = "Audit Service",
+                TaskKey = "Audit"
+            };
+            ILogger logger = new SeriLogger(applicationLoggerOutputSettings, executionContext);
+            IQueueSubscriptionService<JobStatusDto> queueSubscriptionService = new QueueSubscriptionService<JobStatusDto>(queueConfiguration, serializationService, logger);
+            IJobStatusWebServiceCallService<JobStatusDto> jobStatusWebServiceCallService = new JobStatusWebServiceCallService<JobStatusDto>(auditingPersistenceServiceConfig, queueSubscriptionService, logger);
+
+            jobStatusWebServiceCallService.Subscribe();
+
+            logger.LogInfo("Started!");
+
+            ManualResetEvent oSignalEvent = new ManualResetEvent(false);
+            oSignalEvent.WaitOne();
         }
     }
 }
